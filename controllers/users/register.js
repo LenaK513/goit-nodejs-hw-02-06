2b833705ebd6abf8df07d237c8ac3495c1457bdc
Olena <kryvtsovaolena@gmail.com>
2023-01-29 19:37:23 +0200

    get back jwt token@

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,8 +1,8 @@
-// const { Conflict } = require("http-errors");
-// const bcrypt = require("bcrypt");
+const { Conflict } = require("http-errors");
+const bcrypt = require("bcrypt");
 
 const { joiUserSchema } = require("../../models/user");
-// const { User } = require("../../models");
+const { User } = require("../../models");
 
 const register = async (req, res, next) => {
   try {
@@ -12,25 +12,25 @@ const register = async (req, res, next) => {
         message: `${error.message}`,
       });
     }
-    // const { email, password, subscription } = req.body;
-    // const user = await User.findOne({ email });
-    // if (user) {
-    //   next(new Conflict("Email in use"));
-    // }
-    // const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-    // await User.create({
-    //   email,
-    //   password: hashPassword,
-    //   subscription,
-    // });
-    // res.status(201).json({
-    //   data: {
-    //     user: {
-    //       email,
-    //       subscription,
-    //     },
-    //   },
-    // });
+    const { email, password, subscription } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      next(new Conflict("Email in use"));
+    }
+    const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+    await User.create({
+      email,
+      password: hashPassword,
+      subscription,
+    });
+    res.status(201).json({
+      data: {
+        user: {
+          email,
+          subscription,
+        },
+      },
+    });
   } catch (error) {
     next(error);
   }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,70 +1,73 @@
 const express = require("express");
-const passport = require("passport");
-const jwt = require("jsonwebtoken");
+// const passport = require("passport");
+// const jwt = require("jsonwebtoken");
 const router = express.Router();
-const { SECRET_KEY } = process.env;
+// const { SECRET_KEY } = process.env;
 const { user } = require("../../middlewares");
 
 const { users: usersController } = require("../../controllers");
 
 router.post(
   "/register",
-  passport.authenticate("register", { session: false }),
-  async (req, res, next) => {
-    res.json({
-      message: "Register successful",
-      user: req.user,
-    });
-  },
+  // passport.authenticate("register", { session: false }),
+  // async (req, res, next) => {
+  //   res.json({
+  //     message: "Register successful",
+  //     user: req.user,
+  //   });
+  // },
   usersController.register
 );
 
 router.post(
   "/login",
-  passport.authenticate("login", { session: false }),
-  (req, res, next) => {
-    const payload = {
-      id: user._id,
-    };
-    jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" }, (err, token) => {
-      if (err) {
-        return res.json({
-          message: "Failed to login",
-          token: null,
-        });
-      }
-      res.json({ token });
-    });
-  },
+  // passport.authenticate("login", { session: false }),
+  // (req, res, next) => {
+  //   const payload = {
+  //     id: user._id,
+  //   };
+  //   jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" }, (err, token) => {
+  //     if (err) {
+  //       return res.json({
+  //         message: "Failed to login",
+  //         token: null,
+  //       });
+  //     }
+  //     res.json({ token });
+  //   });
+  // },
 
   usersController.login
 );
 
 router.get(
   "/current",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    res.json({ user: req.user });
-  },
+  user,
+  // passport.authenticate("jwt", { session: false }),
+  // (req, res) => {
+  //   res.json({ user: req.user });
+  // },
   usersController.getCurrent
 );
 
 router.post(
   "/logout",
-  passport.authenticate("jwt", { session: false }),
-  (req, res, next) => {
-    res.json({ user: req.user });
-  }
+  user,
+  // passport.authenticate("jwt", { session: false }),
+  // (req, res, next) => {
+  //   res.json({ user: req.user });
+  // }
 
-  // usersController.logout
+  usersController.logout
 );
 
 router.patch(
   "/subscription",
-  passport.authenticate("jwt", { session: false }),
-  (req, res, next) => {
-    res.json({ user: req.user });
-  },
+  user,
+  // passport.authenticate("jwt", { session: false }),
+  // (req, res, next) => {
+  //   res.json({ user: req.user });
+  // },
 
   usersController.updateSubscription
 );
